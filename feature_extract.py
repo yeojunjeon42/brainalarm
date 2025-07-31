300a52efdc962d0df7215bfd472ac494b3dc528d
sleehy <eoungyun@gmail.com>
2025-07-31 17:26:08 +0900

    Add files via upload

---
+++ feature_extract.py
@@ -0,0 +1,37 @@
+import utils
+import yasa
+
+delta = [0.5,4]
+theta = [4,8]
+alpha = [9,11]
+sigma = [12,15]
+beta1 = [14,20]
+beta2 = [20,30]
+gamma1 = [30,40]
+gamma2 = [40,49.5]
+Kcomplex = [0.5,1]
+bands = [delta,theta,alpha,sigma,beta1,beta2,gamma1,gamma2,Kcomplex]
+
+def exfeature(data,fs=512):
+    features = []
+    for band in bands:
+        band_data = utils.filter_bandpass(data, band, fs)
+        pfd = utils.petrosian_fd(band_data) #petrosian fractal dimension
+        SE = utils.spectral_entropy(band_data,fs)
+        SD = utils.standard_deviation(band_data)
+        HA = utils.hjorth_activity(band_data)
+        HM = utils.hjorth_mobility(band_data)
+        HC = utils.hjorth_complexity(band_data)
+        LRSSV = utils.lrssv(band_data)
+        features.extend([pfd,SE,SD,HA,HM,HC,LRSSV])
+    spindles = yasa.spindles_detect(data,sf=1000)
+    if spindles is not None:
+        num_spindle = len(spindles.summary())
+        if num_spindle > 0:
+            yesspindle = 1
+        else:
+            yesspindle = 0
+    else:
+        yesspindle = 0
+    features.append(yesspindle)
+    return features
