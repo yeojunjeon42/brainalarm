dea1b423c3e1e6b10099f73af7aa2edee2c4ba1d
sleehy <eoungyun@gmail.com>
2025-08-03 13:11:24 +0900

    settingtime.py로 시간 설정 기능을 분리하고, alarm.py에서 이를 불러와 사용하도록 수정했습니다.

--- alarm.py
+++ alarm.py
@@ -1,17 +1,9 @@
 import datetime
 import time
 from xgboost.sklearn import XGBClassifier
-from sleep_stage_classifier.feature_extract import exfeature
+from feature_extract import exfeature
+from settingtime import settime, settime_fixed
 
-# 사용자 설정
-wake_time = datetime.datetime.strptime("07:00", "%H:%M").time()  # 기상 목표 시각
-wake_window_min = 15  # ±15분
-start_time = (datetime.datetime.combine(datetime.date.today(), wake_time)
-              - datetime.timedelta(minutes=wake_window_min))  # 탐색 시작 시각
-
-# 모델 로드
-sleep_stage_model = XGBClassifier()
-sleep_stage_model.load_model('xgb_model.json')
 
 # EEG 데이터 수집 및 전처리 함수
 def get_latest_sleep_data():
@@ -21,7 +13,7 @@ def get_latest_sleep_data():
 
 # 알람 작동
 def trigger_alarm():
-    print("알람! 지금 깨면 좋아요 (현재 N2 수면 상태)")
+    print("알람")
     # 실제 알람 구현: 사운드, LED 등 추가 가능
 
 # wake window 내에 있는지 확인
@@ -33,7 +25,7 @@ def is_within_wake_window(current_time, wake_time, window_min=15):
 # 대기 함수
 def wait_until_start(start_datetime):
     print(f"brainalarm 시작 예정 시각: {start_datetime.strftime('%H:%M:%S')}")
-    while datetime.datetime.now() < start_datetime:
+    while datetime.datetime.now() < (start_datetime):
         time.sleep(30)
 
 # 메인 함수
@@ -51,7 +43,6 @@ def smart_alarm_loop():
         if is_within_wake_window(now, wake_time, wake_window_min):
             features = get_latest_sleep_data()
             predicted_stage = sleep_stage_model.predict([features])[0]
-            print(f"[{datetime.datetime.now().strftime('%H:%M:%S')}] 예측 수면 단계: {predicted_stage}")
 
             if predicted_stage == 'N2':
                 trigger_alarm()
@@ -62,5 +53,21 @@ def smart_alarm_loop():
 
         time.sleep(30)  # 30초 간격으로 체크
 
-# 실행
-smart_alarm_loop()
+
+# 사용자 설정
+if settime_fixed:
+    h = (settime // 3600) % 24
+    m = (settime // 60) % 60
+    wake_time = datetime.time((h,m))  # 기상 목표 시각
+    wake_window_min = 30  # 예정 시각 +-30분에서 N2 수면 단계 감지 시 알람 작동
+    start_time = (datetime.datetime.combine(datetime.date.today(), wake_time)
+              - datetime.timedelta(minutes=wake_window_min)) # 탐색 시작 시각
+
+    # 모델 로드
+    sleep_stage_model = XGBClassifier()
+    sleep_stage_model.load_model('xgb_model.json')
+
+
+
+    # 실행
+    smart_alarm_loop()
