ff5934a27c6b31d2002e366489b0f8517323c7b5
sleehy <eoungyun@gmail.com>
2025-08-20 20:58:05 +0900

    .

--- program.py
+++ program.py
@@ -47,7 +47,7 @@ if system.set_time_fixed:
     eeg_reader = EEGReader(port=args.port, baudrate=args.baudrate)
 
     # 모델 로드
-    sleep_stage_model = joblib.load('final_model.joblib')
+    sleep_stage_model = joblib.load('models.final_model.joblib')
 
 
 
