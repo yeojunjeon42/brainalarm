9429cffc2220b28cfe48af8f69d0ecc220da2c15
hjjwa3570 <hjjwa3570@naver.com>
2025-08-26 17:07:05 +0900

    1

--- src/alarm/smart_alarm.py
+++ src/alarm/smart_alarm.py
@@ -47,10 +47,10 @@ def wait_until_start(self, start_datetime):
     print(f"brainalarm 시작 예정 시각: {start_datetime.strftime('%H:%M:%S')}")
     print('start_datetime: ', start_datetime)
     print('현재시각: ',datetime.datetime.now(timezone('Asia/Seoul')).strftime('%H:%M:%S'))
-    self.oled.interface_mode = 'CLOCK'
-    self.oled.update_display()
     while datetime.datetime.now(timezone('Asia/Seoul')) < start_datetime:
         print('waiting until start time...', end='\r')
+        self.oled.interface_mode = 'CLOCK'
+        self.oled.update_display()
         time.sleep(5)
 
 class SmartAlarm:
