66c728848810ac4857d4122dcb5a54ddecee4741
hjjwa3570 <hjjwa3570@naver.com>
2025-08-27 16:16:05 +0900

    .

---
+++ reset_button_test.py
@@ -0,0 +1,10 @@
+import RPi.GPIO as GPIO
+
+reset_pin = 4
+GPIO.setup(reset_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+last_reset_state = GPIO.input(reset_pin)
+while True:
+    reset_state = GPIO.input(reset_pin)
+    if last_reset_state == 1 and reset_state == 0:
+        print("dd")
+    last_reset_state = reset_state
