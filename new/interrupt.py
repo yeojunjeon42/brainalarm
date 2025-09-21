5b6c942d3cf1660167c31f48e4969750d40e4114
sleehy <eoungyun@gmail.com>
2025-09-22 01:39:34 +0900

    .

--- new/interrupt.py
+++ new/interrupt.py
@@ -6,7 +6,7 @@ READ_PIN = 23
 
 GPIO.setmode(GPIO.BCM)
 # 풀다운 저항으로 설정. 버튼을 누르면 1, 안 누르면 0이 됩니다.
-GPIO.setup(READ_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(READ_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 print(f"GPIO {READ_PIN}번 핀 읽기 테스트. (Ctrl+C로 종료)")
 
