c5db814380f36837c50782676bbc59efd0065dc4
sleehy <eoungyun@gmail.com>
2025-09-21 23:22:23 +0900

    .

--- new/main.py
+++ new/main.py
@@ -121,7 +121,7 @@ def main():
         # 3. 종료 단계 (Cleanup)
         # =================================================================
         # 프로그램이 어떤 이유로든 종료될 때 항상 실행됩니다.
-        if eeg_processor.running(): # EEG 스레드가 여전히 실행 중이면 종료
+        if eeg_processor.is_running(): # EEG 스레드가 여전히 실행 중이면 종료
             eeg_processor.stop_collection()
         eeg_processor.disconnect()
         buzzer.stop()
