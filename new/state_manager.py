b1b35680addea685dd9cba53431f6a97748f657c
hjjwa3570 <hjjwa3570@naver.com>
2025-09-22 17:37:39 +0900

    .

--- new/state_manager.py
+++ new/state_manager.py
@@ -108,7 +108,7 @@ class StateManager:
         calculated_window_start_time = window_start_datetime.time()
 
         is_in_window = calculated_window_start_time <= now_time < self.target_time
-        is_target_time = now_time >= self.target_time
+        is_target_time = now >= target_datetime
 
         # 조건 충족 시, 알람을 활성화하고 True를 반환하여 main.py에 알립니다.
         if is_target_time or (is_in_window and sleep_stage == 1): #N2면 1
